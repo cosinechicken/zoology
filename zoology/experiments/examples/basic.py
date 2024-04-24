73f26d656f5c76815f6b1d1aed7e0bf87ad272b1
Brandon Chen <ah.blenden@gmail.com>
2024-04-24 02:48:49 +0000

    Got basic.py working

--- zoology/experiments/examples/basic.py
+++ zoology/experiments/examples/basic.py
@@ -2,16 +2,23 @@ from zoology.config import TrainConfig, ModelConfig, DataConfig, FunctionConfig,
 from zoology.data.associative_recall import MQARConfig
 
 input_seq_len = 64
+model_args = dict(
+    name="zoology.mixers.attention.MHA",
+    kwargs={
+        "dropout": 0.1,
+        "num_heads": 1
+    },
+)
 
 config = TrainConfig(
-    max_epochs=20,
+    max_epochs=40,
     data=DataConfig(
-        train_configs=[MQARConfig(num_examples=10_000, vocab_size=128, input_seq_len=input_seq_len)],
-        test_configs=[MQARConfig(num_examples=1_000, vocab_size=128, input_seq_len=input_seq_len)],# , **factory_kwargs)],
+        train_configs=[MQARConfig(num_examples=10_240, vocab_size=1024, input_seq_len=input_seq_len)],
+        test_configs=[MQARConfig(num_examples=1_024, vocab_size=1024, input_seq_len=input_seq_len)],# , **factory_kwargs)],
     ),
     model=ModelConfig(
-        vocab_size=128,
-        sequence_mixer=ModuleConfig(name="zoology.mixers.attention.MHA")
+        vocab_size=1024,
+        sequence_mixer=ModuleConfig(**model_args)
     ),
 )
 
