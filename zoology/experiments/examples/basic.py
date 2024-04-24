39843aeefab1435b666d4eb4ee7902d313282ba9
Brandon Chen <ah.blenden@gmail.com>
2024-04-24 01:25:05 +0000

    Fix basic.py

--- zoology/experiments/examples/basic.py
+++ zoology/experiments/examples/basic.py
@@ -1,30 +1,18 @@
-from zoology.config import TrainConfig, ModelConfig, DataConfig, FunctionConfig, ModuleConfig
+from zoology.config import TrainConfig, ModelConfig, DataConfig, FunctionConfig, ModuleConfig, DataSegmentConfig
+from zoology.data.associative_recall import MQARConfig
 
-
+input_seq_len = 64
 
 config = TrainConfig(
+    max_epochs=20,
     data=DataConfig(
-        # cache_dir="/path/to/cache/dir"  TODO: add this
-        # HI
-        vocab_size=256,
-        input_seq_len=64,
-        num_train_examples=10_000,
-        num_test_examples=1_000,
-        builder=FunctionConfig(
-            name="zoology.data.associative_recall.multiquery_ar",
-            kwargs={"num_kv_pairs": 4}
-        ),
-        
+        train_configs=[MQARConfig(num_examples=10_000, vocab_size=128, input_seq_len=input_seq_len)],
+        test_configs=[MQARConfig(num_examples=1_000, vocab_size=128, input_seq_len=input_seq_len)],# , **factory_kwargs)],
     ),
     model=ModelConfig(
-        vocab_size=256,
-        max_position_embeddings=64,
-        sequence_mixer=ModuleConfig(
-            name="zoology.mixers.attention.MHA",
-            kwargs={"dropout": 0.1, "num_heads": 1}
-        )
+        vocab_size=128,
+        sequence_mixer=ModuleConfig(name="zoology.mixers.attention.MHA")
     ),
-    
 )
 
 configs = [config]
